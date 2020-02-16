4702c3c5b563f924893aea877eb96990cc5f7c82
Ryan Compton <ryan.wade.compton@gmail.com>
2020-02-16 15:22:38 -0700

    Revise resume. Add my info to footers throughout htmls.

---
+++ portfolioProjects.js
@@ -0,0 +1,27 @@
+const projects = [
+
+    {
+        projectTitle: "Weather Dashboard",
+        deployedLink: "https://comp7589.github.io/WeatherDashboard/",
+        gitHubRepo: "https://github.com/comp7589/WeatherDashboard",
+        screenShot: "#",
+        description: "Built using JavaScript, HTML, CSS, JQuery, & OpenWeather API. Application takes in a search city and displays current weather, as well as five-day forecast."
+    },
+
+    {
+        projectTitle: "Day Planner",
+        deployedLink: "https://comp7589.github.io/WorkDayScheduler/",
+        gitHubRepo: "https://github.com/comp7589/WorkDayScheduler",
+        screenShot: "#",
+        description: "Built using JavaScript, HTML, CSS, & JQuery. Application lets user schedule, and save, events throughout the day from 9:00am - 5:00pm."
+    },
+
+    {
+        projectTitle: "Media Query + Player Lyric Generator",
+        deployedLink: "https://sk2060sk2060.github.io/group-project-1/",
+        gitHubRepo: "https://github.com/sk2060sk2060/group-project-1",
+        screenShot: "#",
+        description: "Built using JavaScript, HTML, CSS, & JQuery. Application lets user search for a song, or artist, and top seven results are displayed. User may then click a song in media player to listen and get lyrics to track."
+    }
+]
+
